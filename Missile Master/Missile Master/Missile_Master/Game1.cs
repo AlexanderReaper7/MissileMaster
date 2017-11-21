7056e3b715228da11bf1713732d8b9ae7bbb3d8a
Alexander Öberg <stativetroller7@gmail.com>
2017-11-21 16:30:49 +0100

    Auto stash before merge of "master" and "MissileMaster/master"

--- Missile Master/Missile Master/Missile_Master/Game1.cs
+++ Missile Master/Missile Master/Missile_Master/Game1.cs
@@ -13,10 +13,15 @@ namespace Missile_Master
 {
     public class Game1 : Microsoft.Xna.Framework.Game
     {
-        #region Variables
-
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        #region Variables
+
+        #region Floats
+        const float defaultTurnRate = 0.6f;
+        const float defaultThrust = 10f;
+        #endregion
 
         #region 2DTextures
         Texture2D MainMenuBG;
@@ -44,6 +49,23 @@ namespace Missile_Master
         SpriteFont RobotoRegular36;
         #endregion
 
+        #region Vectors
+
+        Vector2 playerSpeed = new Vector2(0, 0);
+        Vector2 playerPosition = new Vector2(0, 0);
+
+        #endregion
+
+        #region UpgradeStates
+        float mainThrusterLVL = 1f;
+        int bodyLVL = 1;
+        float sideThrusterLVL = 1f;
+        int payloadLVL = 1;
+        int fuelLVL = 1;
+        int DecoyLVL = 1;
+        #endregion
+
+        #region States
         //gamestates
         enum GameStates
         {
@@ -62,6 +84,17 @@ namespace Missile_Master
 
         //starting gamestate
         GameStates gameState = GameStates.MainMenu;
+
+        //RotationStates
+        enum RotationStates
+        {
+            D0,D45,D90,D135,D180,D225,D270,D315 //D for degrees
+        };
+
+        //starting RotationState
+        RotationStates rotationstate = RotationStates.D0;
+        #endregion 
+
         #endregion
 
         public Game1()
@@ -70,12 +103,6 @@ namespace Missile_Master
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -199,6 +226,7 @@ namespace Missile_Master
 
                 #region Level1
                 case GameStates.Level1:
+                    Controls(gameTime);
                     break;
                 #endregion
 
@@ -236,14 +264,169 @@ namespace Missile_Master
 
             base.Update(gameTime);
         }
-        public void ESCtoMainMenu ()
+
+        public void ESCtoMainMenu()
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape) || Keyboard.GetState().IsKeyDown(Keys.Back)) //esc or backspace to return to Mainmenu
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape) || Keyboard.GetState().IsKeyDown(Keys.Back)) //esc or back to return to Mainmenu
             {
                 gameState = GameStates.MainMenu;
             }
-
         }
+
+        public void Controls(GameTime gameTime) //---------------IMPORTANT----------------------------------------------
+        {
+            float mainThrusterPower = mainThrusterLVL * 30;
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                //accelerate
+                switch (rotationstate)
+                {
+                    case RotationStates.D0:
+                        //TODO lägg till luftmotstånd
+                        playerSpeed.Y += defaultThrust * mainThrusterLVL;
+                        break;
+
+                    case RotationStates.D45:
+                        playerSpeed.Y += (defaultThrust * mainThrusterLVL) / 2;
+                        playerSpeed.X += (defaultThrust * mainThrusterLVL) / 2;
+                        break;
+
+                    case RotationStates.D90:
+                        playerSpeed.X += defaultThrust * mainThrusterLVL;
+                        break;
+
+                    case RotationStates.D135:
+                        playerSpeed.Y += ((defaultThrust * mainThrusterLVL) / 2) * -1;
+                        playerSpeed.X += (defaultThrust * mainThrusterLVL) / 2;
+                        break;
+
+                    case RotationStates.D180:
+                        playerSpeed.Y += defaultThrust * mainThrusterLVL * -1;
+                        break;
+
+                    case RotationStates.D225:
+                        playerSpeed.Y += (defaultThrust * mainThrusterLVL) / 8;
+                        playerSpeed.X += (defaultThrust * mainThrusterLVL) / 8;
+                        break;
+
+                    case RotationStates.D270:
+                        playerSpeed.X += (defaultThrust * mainThrusterLVL) / 8;
+                        break;
+
+                    case RotationStates.D315:
+                        playerSpeed.Y += (defaultThrust * mainThrusterLVL) * 1;
+                        playerSpeed.X += (defaultThrust * mainThrusterLVL) * 7;
+                        break;
+                }
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                //turn counter-clockwise
+                float countDuration = defaultTurnRate / sideThrusterLVL; //time until switch rotationstate
+                float currentTime = 0f;
+
+                currentTime += (float)gameTime.ElapsedGameTime.TotalSeconds; //Time passed since last Update() 
+
+                if (currentTime >= countDuration)
+                {
+                    switch (rotationstate)
+                    {
+                        case RotationStates.D0:
+                            rotationstate = RotationStates.D315;
+                            break;
+
+                        case RotationStates.D45:
+                            rotationstate = RotationStates.D0;
+                            break;
+
+                        case RotationStates.D90:
+                            rotationstate = RotationStates.D45;
+                            break;
+
+                        case RotationStates.D135:
+                            rotationstate = RotationStates.D90;
+                            break;
+
+                        case RotationStates.D180:
+                            rotationstate = RotationStates.D135;
+                            break;
+
+                        case RotationStates.D225:
+                            rotationstate = RotationStates.D180;
+                            break;
+
+                        case RotationStates.D270:
+                            rotationstate = RotationStates.D225;
+                            break;
+
+                        case RotationStates.D315:
+                            rotationstate = RotationStates.D270;
+                            break;
+
+                    }
+                    currentTime -= countDuration; // "use up" the time
+                    
+                }
+
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                //decelerate
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                // turn clockwise
+                float countDuration = defaultTurnRate / sideThrusterLVL; //every  2s.
+                float currentTime = 0f;
+
+                currentTime += (float)gameTime.ElapsedGameTime.TotalSeconds; //Time passed since last Update() 
+
+                if (currentTime >= countDuration)
+                {
+                    switch (rotationstate)
+                    {
+                        case RotationStates.D0:
+                            rotationstate = RotationStates.D45;
+                            break;
+
+                        case RotationStates.D45:
+                            rotationstate = RotationStates.D90;
+                            break;
+
+                        case RotationStates.D90:
+                            rotationstate = RotationStates.D135;
+                            break;
+
+                        case RotationStates.D135:
+                            rotationstate = RotationStates.D180;
+                            break;
+
+                        case RotationStates.D180:
+                            rotationstate = RotationStates.D225;
+                            break;
+
+                        case RotationStates.D225:
+                            rotationstate = RotationStates.D270;
+                            break;
+
+                        case RotationStates.D270:
+                            rotationstate = RotationStates.D315;
+                            break;
+
+                        case RotationStates.D315:
+                            rotationstate = RotationStates.D0;
+                            break;
+
+                    }
+                    currentTime -= countDuration; // "use up" the time
+
+                }
+            }
+        }
+
         protected override void Draw(GameTime gameTime)
         {
             spriteBatch.Begin();
@@ -380,8 +563,6 @@ namespace Missile_Master
             }
 
             spriteBatch.End();
-                // TODO: Add your drawing code here
-
                 base.Draw(gameTime);
             
         }
